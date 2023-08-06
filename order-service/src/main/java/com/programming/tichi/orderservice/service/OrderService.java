a7bae87036b358b627cb37208c187270eb589e2e
pankaj <32pankaj@gmail.com>
2023-08-06 17:57:40 +0530

    3rdnd commit

--- order-service/src/main/java/com/programming/tichi/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programming/tichi/orderservice/service/OrderService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,34 +24,37 @@ public class OrderService {
 
     @Autowired
     WebClient webClient;
-    public void placeOrder(OrderRequest orderRequest){
-        Order order=new Order();
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto).toList();
 
         order.setOrderLineItemList(orderLineItems);
+        List<String>skuCodes=order.getOrderLineItemList().stream().map(orderLineItem -> orderLineItem.getSkuCode()).toList();
 
         // call inventory Service and place order if product is in
-//        stock
+        //        stock
 
-      Boolean Result=  webClient.get()
-                .uri("http://localhost:8082/api/inventory")
+        Boolean Result = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        UriBuilder->UriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(Boolean.class)
-                                .block();
+                .block();
 
         if (Result) {
             orderRepository.save(order);
-        }else {
+        } else {
             throw new IllegalArgumentException("Product is  not in stock , please try again");
         }
 
     }
 
     private OrderLineItem mapToDto(OrderLineItemsDto orderLineItemsDto) {
-        OrderLineItem orderLineItem=new OrderLineItem();
+        OrderLineItem orderLineItem = new OrderLineItem();
         orderLineItem.setPrice(orderLineItemsDto.getPrice());
         orderLineItem.setQuantity(orderLineItemsDto.getQuantity());
         orderLineItem.setSkuCode(orderLineItemsDto.getSkuCode());
